2f0bde28aa0e7590fc960e595e0bd904e9841230
Galata Noel <galatanoel@tutanota.com>
2025-05-28 07:42:25 +0200

    fixes

--- src/main/java/org/example/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/exception/GlobalExceptionHandler.java
@@ -2,10 +2,8 @@ package org.example.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
--- src/main/java/org/example/exception/ResourceNotFoundException.java
+++ src/main/java/org/example/exception/ResourceNotFoundException.java
@@ -1,11 +1,5 @@
 package org.example.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-@ControllerAdvice
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
