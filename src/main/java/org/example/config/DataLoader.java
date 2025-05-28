5e55de1eb6ff7af9f84c54655fd15c913df2d431
Galata Noel <galatanoel@tutanota.com>
2025-05-28 07:53:38 +0200

    fixes

--- src/main/java/org/example/config/DataLoader.java
+++ src/main/java/org/example/config/DataLoader.java
@@ -5,29 +5,46 @@ import org.example.model.Book;
 import org.example.repository.AuthorRepository;
 import org.example.repository.BookRepository;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
-public class DataLoader {
+public class DataLoader implements CommandLineRunner {
 
-    @Bean
-    public CommandLineRunner initDatabase(AuthorRepository authorRepo, BookRepository bookRepo) {
-        return args -> {
-            Author author1 = new Author();
-            author1.setName("J.K. Rowling");
-            authorRepo.save(author1);
+    private final AuthorRepository authorRepo;
+    private final BookRepository bookRepo;
 
-            Book book1 = new Book();
-            book1.setTitle("Harry Potter és a bölcsek köve");
-            book1.setIsbn("963-8386-87-0");
-            book1.setAuthor(author1);
-            bookRepo.save(book1);
+    public DataLoader(AuthorRepository authorRepo, BookRepository bookRepo) {
+        this.authorRepo = authorRepo;
+        this.bookRepo = bookRepo;
+    }
 
-            Author author2 = new Author();
-            author2.setName("Stephen King");
-            authorRepo.save(author2);
-        };
+    @Override
+    public void run(String... args) {
+        // Először töröljük a meglévő adatokat a tiszta kezdéshez
+        bookRepo.deleteAll();
+        authorRepo.deleteAll();
+
+        try {
+            // J.K. Rowling és könyve
+            Author rowling = new Author();
+            rowling.setName("J.K. Rowling");
+            rowling = authorRepo.save(rowling);
+
+            Book harryPotter = new Book();
+            harryPotter.setTitle("Harry Potter és a bölcsek köve");
+            harryPotter.setIsbn("963-8386-87-0");
+            harryPotter.setAuthor(rowling);
+            bookRepo.save(harryPotter);
+
+            // Stephen King
+            Author king = new Author();
+            king.setName("Stephen King");
+            authorRepo.save(king);
+
+            System.out.println("Adatbázis inicializálása sikeres!");
+        } catch (Exception e) {
+            System.err.println("Hiba az adatbázis inicializálása során: " + e.getMessage());
+            throw e;
+        }
     }
 }
